8af0dcd60d20899b5f11db2f4012a3f43d0c2546
Sinth <sinthmite@gmail.com>
2024-03-22 18:01:51 -0400

    finished

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ slidingOptions = document.getElementById("Slidingoptions");
 menu = document.getElementById("menu");
 totalContainer = document.getElementById("totalContainer");
 remove = document.getElementById("remove");
+overlay = document.getElementById("overlay");
 menu.addEventListener("click", function() {
     slidingOptions.style.display = "block";
     document.body.style.overflow = "hidden";
