bc0b6f1beb7aa9b36016bf06e6fb81660fcb5cc5
Sinth <sinthmite@gmail.com>
2024-03-22 00:35:07 -0400

    Most finished

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ totalContainer = document.getElementById("totalContainer");
 remove = document.getElementById("remove");
 menu.addEventListener("click", function() {
     slidingOptions.style.display = "block";
-    // Disable scrolling when menu is open
     document.body.style.overflow = "hidden";
+
 });
 
 remove.addEventListener("click", function() {
     slidingOptions.style.display = "none";
-    // Enable scrolling when menu is closed
-    document.body.style.overflow = "auto"; // You can adjust this to 'scroll' if you always want to show the scrollbar
+    document.body.style.overflow = "auto"; 
+
 });
